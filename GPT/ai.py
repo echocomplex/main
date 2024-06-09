20c04232bc6a49f7cc926ff3072532b8b677dd21
root <root@AliceWNPC>
2024-06-10 02:04:31 +0300

    Update

--- GPT/ai.py
+++ GPT/ai.py
@@ -5,9 +5,8 @@ Using g4f from gpt4free by xtekky (https://github.com/xtekky/gpt4free/)
 """
 
 """ IMPORTS """
-import g4f;
-from g4f.Provider import *;
-
+from g4f.client import Client
+from g4f.Provider import DeepInfra
 class ChatGPT:
     def __init__ (self) -> None:
         self.__messages: list = [];
@@ -16,31 +15,19 @@ class ChatGPT:
         self.__messages.append({"role": "assistant", "content": message});
 
     def generate_response(self, question: str) -> str:
-        self.__messages.append({"role": "user", "content": question})
-        stat: int = 0;
-        while (True):
-            if (stat > 5):
-                raise Exception("Something went wrong... Try again later.");
-            try:
-                response = g4f.ChatCompletion.create(
-                    model=g4f.models.gpt_35_turbo,
-                    messages=self.__messages
-                );
-            except Exception as e:
-                print(e);
-                stat: int = 0;
-                continue;
-            if (('<!DOCTYPE html>' in response) or ("流量异常" in response) or (response == "") or ("chatbase" in response.lower())):
-                stat += 1;
-                continue;
-            else:
-                break;
-        return response;
+        self.__messages.append({"role": "user", "content": question});
+        client = Client();
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            provider=DeepInfra,
+            messages=self.__messages
+        )
+        return response.choices[0].message.content;
 
 
 
 if (__name__ == "__main__"):
     chat = ChatGPT();
     chat.add_bot_message("");
-    print(chat.generate_response("Hello!"));
+    print(chat.generate_response("Hello! How are you?"));
 
